e65874fccc555901e372ba11fc6f763d7b1bfc10
Afzal14786 <mdafzal14777@gmail.com>
2025-09-28 03:05:53 +0530

    Day 1 Setup Done --target completed

---
+++ README.md
@@ -0,0 +1,4 @@
+# iamafzal -- a techie (Landing Page)
+
+This file provides a minimal setup to get project working in Vite with HMR and some ESLint rules.
+
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='m-5 text-center'>This is the first page baby</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
